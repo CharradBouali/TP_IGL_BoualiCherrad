e359359e24721ac06550919aa7becae6c8c230f1
CharradBouali <32856980+CharradBouali@users.noreply.github.com>
2017-10-25 17:36:30 +0200

    Update Trier_Test.java

--- src/Trier_Test.java
+++ src/Trier_Test.java
@@ -7,13 +7,14 @@ public class Trier_Test {
 	@Test
 	public void Trier_test() 
 	{
-		VectorHelper a=new VectorHelper(4);
-		int []res=new int[4];
+		VectorHelper a=new VectorHelper(5);
+		int []res=new int[5];
 		
 		a.affect(13, 0);
 		a.affect(12, 1);
 		a.affect(21, 2);
 		a.affect(6, 3);
+		a.affect(33, 4);
 		
 		res=a.trier();
 		
@@ -21,6 +22,7 @@ public class Trier_Test {
 		assertEquals(res[1],12);
 		assertEquals(res[2],13);
 		assertEquals(res[3],21);
+		assertEquals(res[4],33);
 		
 	}
 
